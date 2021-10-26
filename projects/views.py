39d16a1c369a6a9a0e493323b912667b5b1a4296
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-10-26 16:41:25 +0000

    added projects views and templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
+
+def all_projects(request):
+    """ A view to return the projects page with all its functionalities """
+    projects = Project.objects.all()
+
+    context = {
+        'projects': projects,
+    }
+
+
+    return render(request, 'projects/projects.html', context)
