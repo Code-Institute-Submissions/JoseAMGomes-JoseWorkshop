046ef8c6acd3543b26093f65c6978fb0cb9eac44
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-10-26 16:23:58 +0000

    costumized admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Project
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'rating',
+        'images',
+    )
+
+    ordering = ('sku',)
+
+
 # Register your models here.
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Project(models.Model):
-   
+    
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
