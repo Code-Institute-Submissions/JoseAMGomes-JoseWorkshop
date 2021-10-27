dc2a0c64a45deb7d0720a889f7f368d2dc0f751b
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-10-27 00:27:24 +0000

    added views and template for individual project pages

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_projects, name='projects')
+    path('', views.all_projects, name='projects'),
+    path('<project_id>', views.project_detail, name='project_detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
+
 # Create your views here.
 
 def all_projects(request):
-    """ A view to return the projects page with all its functionalities """
+    """ A view to show all projects, including sorting and search queries """
+
     projects = Project.objects.all()
 
     context = {
         'projects': projects,
     }
 
-
     return render(request, 'projects/projects.html', context)
+
+
+def project_detail(request, project_id):
+    """ A view to show individual project details """
+
+    project = get_object_or_404(Project, pk=project_id)
+
+    context = {
+        'project': project,
+    }
+
+    return render(request, 'projects/project_detail.html', context)
