082e677f288e1233201e19972a4f9b1e365c536c
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-10-27 14:36:28 +0000

    added search funtionality and sort items funtionality

--- projects/views.py
+++ projects/views.py
@@ -1,17 +1,51 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Project
-
 # Create your views here.
 
 def all_projects(request):
     """ A view to show all projects, including sorting and search queries """
 
     projects = Project.objects.all()
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                projects = projects.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            projects = projects.order_by(sortkey)
+
+
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('projects'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            projects = projects.filter(queries)
+
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'projects': projects,
+        'search_term': query,
+        'current_sorting': current_sorting,
     }
-
     return render(request, 'projects/projects.html', context)
 
 
