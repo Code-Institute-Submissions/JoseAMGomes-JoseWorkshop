992a7f4424b3c1a210a719d72b5db7a94a7dc69d
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-10-28 01:54:24 +0000

    added add to bag funtionality initial code. not yet working

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,10 +1,24 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from projects.models import Project
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     project_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id in bag.items():
+        project = get_object_or_404(Project, pk=item_id)
+        total += project.price
+        project_count += 1
+        bag_items.append({
+            'item_id': item_id,
+            'project': project,
+        })
+
+
     
     context = {
         'bag_items': bag_items,
