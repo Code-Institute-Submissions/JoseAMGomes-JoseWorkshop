09c7aef51e8dc5ffbf3a56c9ca7ef44e3fae788f
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-10-29 02:02:54 +0000

    added toasts funtionality together with preview

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from projects.models import Project
 
 # Create your views here.
 
@@ -10,25 +12,37 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = int(1)
+    project = Project.objects.get(pk=item_id)
+    
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    bag[item_id] = 1
+    messages.success(request, f'Added {project.name} to your bag')
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
 
     request.session['bag'] = bag
-    return redirect(redirect_url)
+    return redirect(reverse('projects'))
 
 
 def remove_bag(request, item_id):
     """Remove the item from the shopping bag"""
+    project = Project.objects.get(pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     bag.pop(item_id)
-       
+    messages.success(request, f'Removed {project.name} from your bag')   
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+    try:
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+        messages.success(request, f'Removed {project.name} from your bag')
+        request.session['bag'] = bag
+        return redirect(reverse('view_bag'))
+
+    except Exception as e:
+        messages.error(request, f'Error found! {e} ')
+        return HttpResponse(status=500)
