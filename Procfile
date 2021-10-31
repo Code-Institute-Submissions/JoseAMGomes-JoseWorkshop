d2a2cc2a4657de107f35d47488ac9ac84ea67dff
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-10-31 00:06:23 +0000

    continued deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn joseworkshop.wsgi:application
--- joseworkshop/settings.py
+++ joseworkshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$zank!k(#5ueeu9qek-fkw97n6z^+(+)t#z5w!agx64tl-owa&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joseworkshop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,13 +118,17 @@ WSGI_APPLICATION = 'joseworkshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
